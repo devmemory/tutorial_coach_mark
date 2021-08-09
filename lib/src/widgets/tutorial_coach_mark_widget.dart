2dfc096a9bc5c71771293823ae30c69524b94ce1
devmemory <apsapo12@gmail.com>
2021-08-09 13:14:53 +0900

    - remove padding only

--- lib/src/widgets/tutorial_coach_mark_widget.dart
+++ lib/src/widgets/tutorial_coach_mark_widget.dart
@@ -145,7 +145,7 @@ class TutorialCoachMarkWidgetState extends State<TutorialCoachMarkWidget>
     haloWidth = haloWidth * 0.6 + widget.paddingFocus;
     haloHeight = haloHeight * 0.6 + widget.paddingFocus;
 
-    // double weight = 0.0;
+    double weight = 0.0;
     double? top;
     double? bottom;
     double? left;
@@ -154,7 +154,7 @@ class TutorialCoachMarkWidgetState extends State<TutorialCoachMarkWidget>
       switch (i.align) {
         case ContentAlign.bottom:
           {
-            // weight = MediaQuery.of(context).size.width;
+            weight = MediaQuery.of(context).size.width;
             left = 0;
             top = positioned.dy + haloHeight;
             bottom = null;
@@ -162,7 +162,7 @@ class TutorialCoachMarkWidgetState extends State<TutorialCoachMarkWidget>
           break;
         case ContentAlign.top:
           {
-            // weight = MediaQuery.of(context).size.width;
+            weight = MediaQuery.of(context).size.width;
             left = 0;
             top = null;
             bottom = haloHeight +
@@ -171,7 +171,7 @@ class TutorialCoachMarkWidgetState extends State<TutorialCoachMarkWidget>
           break;
         case ContentAlign.left:
           {
-            // weight = positioned.dx - haloWidth;
+            weight = positioned.dx - haloWidth;
             left = 0;
             top = positioned.dy - target.size.height / 2 - haloHeight;
             bottom = null;
@@ -182,7 +182,7 @@ class TutorialCoachMarkWidgetState extends State<TutorialCoachMarkWidget>
             left = positioned.dx + haloWidth;
             top = positioned.dy - target.size.height / 2 - haloHeight;
             bottom = null;
-            // weight = MediaQuery.of(context).size.width - left!;
+            weight = MediaQuery.of(context).size.width - left!;
           }
           break;
         case ContentAlign.custom:
@@ -190,7 +190,7 @@ class TutorialCoachMarkWidgetState extends State<TutorialCoachMarkWidget>
             left = i.customPosition!.left;
             top = i.customPosition!.top;
             bottom = i.customPosition!.bottom;
-            // weight = MediaQuery.of(context).size.width;
+            weight = MediaQuery.of(context).size.width;
           }
           break;
       }
@@ -199,9 +199,12 @@ class TutorialCoachMarkWidgetState extends State<TutorialCoachMarkWidget>
         top: top,
         bottom: bottom,
         left: left,
-        child: (i.builder != null
-            ? i.builder?.call(context, this)
-            : (i.child ?? SizedBox.shrink()))!,
+        child: SizedBox(
+          width: weight,
+          child: (i.builder != null
+              ? i.builder?.call(context, this)
+              : (i.child ?? SizedBox.shrink()))!,
+        ),
         // child: Container(
         //   width: weight,
         //   child: Padding(
@@ -215,7 +218,6 @@ class TutorialCoachMarkWidgetState extends State<TutorialCoachMarkWidget>
     }).toList();
 
     return Stack(
-      fit: StackFit.expand,
       children: children,
     );
   }
